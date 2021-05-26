6e77634492295e1ee277d0bad46749ecd28faf57
glauconeto <neto.glauquinho@gmail.com>
2021-05-26 14:21:57 -0300

    alterei a query

--- query.sql
+++ query.sql
@@ -1,50 +1,9 @@
--- MySQL dump 10.13  Distrib 8.0.25, for Linux (x86_64)
---
--- Host: localhost    Database: db_tcc
--- ------------------------------------------------------
--- Server version	8.0.25-0ubuntu0.20.04.1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `categorias`
---
-
-DROP TABLE IF EXISTS `categorias`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categorias` (
   `id_categoria` int NOT NULL AUTO_INCREMENT,
   `nome_categoria` varchar(100) NOT NULL,
   PRIMARY KEY (`id_categoria`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `categorias`
---
-
-LOCK TABLES `categorias` WRITE;
-/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
-/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `comerciante`
---
-
-DROP TABLE IF EXISTS `comerciante`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `comerciante` (
   `id_comerciante` int NOT NULL AUTO_INCREMENT,
   `cnpj` varchar(17) DEFAULT NULL,
@@ -54,25 +13,8 @@ CREATE TABLE `comerciante` (
   PRIMARY KEY (`id_comerciante`),
   KEY `ce_ComercianteComercio` (`id_comercio`),
   CONSTRAINT `ce_ComercianteComercio` FOREIGN KEY (`id_comercio`) REFERENCES `comercio` (`id_comercio`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `comerciante`
---
-
-LOCK TABLES `comerciante` WRITE;
-/*!40000 ALTER TABLE `comerciante` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comerciante` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `comercio`
---
-
-DROP TABLE IF EXISTS `comercio`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `comercio` (
   `id_comercio` int NOT NULL AUTO_INCREMENT,
   `nome_comercio` varchar(120) NOT NULL,
@@ -85,25 +27,8 @@ CREATE TABLE `comercio` (
   PRIMARY KEY (`id_comercio`),
   KEY `ce_CategoriaComercio` (`id_categoria`),
   CONSTRAINT `ce_CategoriaComercio` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `comercio`
---
-
-LOCK TABLES `comercio` WRITE;
-/*!40000 ALTER TABLE `comercio` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comercio` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `favorito`
---
-
-DROP TABLE IF EXISTS `favorito`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `favorito` (
   `id_favorito` int NOT NULL,
   `favorito` text,
@@ -114,25 +39,8 @@ CREATE TABLE `favorito` (
   KEY `id_comercio` (`id_comercio`),
   CONSTRAINT `favorito_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
   CONSTRAINT `favorito_ibfk_2` FOREIGN KEY (`id_comercio`) REFERENCES `comercio` (`id_comercio`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `favorito`
---
-
-LOCK TABLES `favorito` WRITE;
-/*!40000 ALTER TABLE `favorito` DISABLE KEYS */;
-/*!40000 ALTER TABLE `favorito` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuario`
---
-
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
   `id_usuario` int NOT NULL,
   `nome` varchar(50) NOT NULL,
@@ -142,35 +50,10 @@ CREATE TABLE `usuario` (
   PRIMARY KEY (`id_usuario`),
   KEY `ce_UsuarioFavorito` (`id_favorito`),
   CONSTRAINT `ce_UsuarioFavorito` FOREIGN KEY (`id_favorito`) REFERENCES `favorito` (`id_favorito`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
-CREATE TABLE recuperacao (
-  utilizador  VARCHAR(255) NOT NULL,
-  confirmacao VARCHAR(40) NOT NULL,
-  KEY(utilizador, confirmacao)
-)
-
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping routines for database 'db_tcc'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-05-26 11:48:15
+-- CREATE TABLE recuperacao (
+--   utilizador  VARCHAR(255) NOT NULL,
+--   confirmacao VARCHAR(40) NOT NULL,
+--   KEY(utilizador, confirmacao)
+-- )
