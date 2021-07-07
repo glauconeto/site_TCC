ddefcd2b1ee1eea7c66798fa2c1f2909f635913b
glauconeto <neto.glauquinho@gmail.com>
2021-07-07 15:04:07 -0300

    alterei a query

--- query.sql
+++ query.sql
@@ -1,55 +1,41 @@
-USE db_tcc;
+CREATE TABLE comerciante (
+  id_comerciante int NOT NULL AUTO_INCREMENT,
+  nome_comerciante varchar(100) DEFAULT NULL,
+  email varchar(250) DEFAULT NULL,
+  CNPJ varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (id_comerciante)
+);
 
-DROP TABLE IF EXISTS `comerciante`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `comerciante` (
-  `id_comerciante` int NOT NULL AUTO_INCREMENT,
-  `nome_comerciante` varchar(100) DEFAULT NULL,
-  `email` varchar(250) DEFAULT NULL,
-  `CNPJ` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  PRIMARY KEY (`id_comerciante`)
-) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE comercio (
+  id_comercio int NOT NULL AUTO_INCREMENT,
+  nome_comercio varchar(120) NOT NULL,
+  endereco varchar(150) NOT NULL,
+  telefone varchar(19) NOT NULL,
+  facebook varchar(250) DEFAULT NULL,
+  celular varchar(19) DEFAULT NULL,
+  descricao text NOT NULL,
+  categoria varchar(100) NOT NULL,
+  id_comerciante int NOT NULL,
+  PRIMARY KEY (id_comercio),
+  KEY ce_ComercianteComercio (id_comerciante),
+  CONSTRAINT ce_ComercianteComercio FOREIGN KEY (id_comerciante) REFERENCES comerciante (id_comerciante) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-DROP TABLE IF EXISTS `comercio`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `comercio` (
-  `id_comercio` int NOT NULL AUTO_INCREMENT,
-  `nome_comercio` varchar(120) NOT NULL,
-  `endereco` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `telefone` varchar(19) NOT NULL,
-  `facebook` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `celular` varchar(19) DEFAULT NULL,
-  `descricao` text NOT NULL,
-  `categoria` varchar(100) NOT NULL,
-  `id_comerciante` int NOT NULL,
-  PRIMARY KEY (`id_comercio`),
-  KEY `ce_ComercianteComercio` (`id_comerciante`),
-  CONSTRAINT `ce_ComercianteComercio` FOREIGN KEY (`id_comerciante`) REFERENCES `comerciante` (`id_comerciante`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE favorito (
+  id_favorito int NOT NULL AUTO_INCREMENT,
+  id_usuario int DEFAULT NULL,
+  id_comercio int DEFAULT NULL,
+  PRIMARY KEY (id_favorito),
+  KEY id_usuario (id_usuario),
+  KEY id_comercio (id_comercio),
+  CONSTRAINT favorito_ibfk_1 FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario),
+  CONSTRAINT favorito_ibfk_2 FOREIGN KEY (id_comercio) REFERENCES comercio (id_comercio)
+);
 
-DROP TABLE IF EXISTS `favorito`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `favorito` (
-  `id_favorito` int NOT NULL AUTO_INCREMENT,
-  `id_usuario` int DEFAULT NULL,
-  `id_comercio` int DEFAULT NULL,
-  PRIMARY KEY (`id_favorito`),
-  KEY `id_usuario` (`id_usuario`),
-  KEY `id_comercio` (`id_comercio`),
-  CONSTRAINT `favorito_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
-  CONSTRAINT `favorito_ibfk_2` FOREIGN KEY (`id_comercio`) REFERENCES `comercio` (`id_comercio`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuario` (
-  `id_usuario` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) NOT NULL,
-  `senha` varchar(255) NOT NULL,
-  `criado_em` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE usuario (
+  id_usuario int NOT NULL AUTO_INCREMENT,
+  nome varchar(50) NOT NULL,
+  senha varchar(255) NOT NULL,
+  criado_em datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id_usuario)
+);
