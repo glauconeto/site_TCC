67b6808ee50baa62dd404798f9abffb1396b91c0
glauco neto <neto.glauquinho@gmail.com>
2021-07-31 10:57:45 -0300

    corrigi estilo de erro

--- templates/login.php
+++ templates/login.php
@@ -7,13 +7,13 @@
     <div class="py-5 text-center align-items-center d-flex" style="background-image: linear-gradient(to left bottom, rgba(189, 195, 199, .75), rgba(44, 62, 80, .75)); background-size: 100%; left: 20em;">
         <div class="container">
             <div class="row wrapper">
-                    <?php 
-                        if(!empty($login_err)){
-                            echo '<div class="alert alert-danger" style="margin: 1; left: 38px; 0px 7px;">' . $login_err . '</div>';
-                        }
-                    ?>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" style="padding-left: 20em;">
                         <h1><i class="fa fa-user" aria-hidden="true"></i></h1>
+                        <?php 
+                            if(!empty($login_err)){
+                                echo '<div class="alert alert-danger">' . $login_err . '</div>';
+                            }
+                        ?>
                         <div class="form-group">
                             <label>Nome de usuário</label>
                             <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>" style="width: 500px;left: 450px;position: sticky;">
